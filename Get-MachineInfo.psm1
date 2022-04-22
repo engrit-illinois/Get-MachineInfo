43adb6b0c51300bb083deefae5090f6be4f0e248
mmseng <mmseng@users.noreply.github.com>
2022-04-22 14:07:02 -0500

    Implemented asynchronicity

---
+++ Get-MachineInfo.psm1
@@ -0,0 +1,126 @@
+# Documentation home: https://github.com/engrit-illinois/Get-MachineInfo
+function Get-MachineInfo {
+	
+	param(
+		[Parameter(Mandatory=$true,Position=0)]
+		[string[]]$ComputerName,
+		
+		[string]$OUDN = "OU=Desktops,OU=Engineering,OU=Urbana,DC=ad,DC=uillinois,DC=edu",
+		
+		[string]$CIMTimeoutSec = 30,
+		
+		[switch]$Log,
+		
+		[switch]$Csv,
+		
+		[string]$LogDir = "c:\engrit\logs",
+		
+		[int]$ThrottleLimit = 50
+	)
+	
+	$ts = Get-Date -Format "yyyy-MM-dd_HH-mm-ss"
+	$LOGPATH = "$LogDir\Get-Model_$ts.log"
+	$CSVPATH = $LOGPATH.Replace(".log",".csv")
+	
+	function log {
+		param(
+			[string]$Msg,
+			[switch]$NoTS
+		)
+		
+		$ts = Get-Date -Format "yyyy-MM-dd_HH-mm-ss"
+		if(!$NoTS) {
+			$msg = "[$ts] $msg"
+		}
+		
+		Write-Host $Msg
+		
+		if($Log) {
+			if(!(Test-Path -PathType Leaf -Path $LOGPATH)) {
+				$shutup = New-Item -ItemType File -Force -Path $LOGPATH
+			}
+			$Msg | Out-File $LOGPATH -Append
+		}
+	}
+
+	function Get-Comps {
+		$comps = @()
+		foreach($query in @($ComputerName)) {
+			$thisQueryComps = (Get-ADComputer -Filter "name -like '$query'" -SearchBase $OUDN | Select Name).Name
+			$comps += @($thisQueryComps)
+		}
+		$comps
+	}
+	
+	function Get-Data($comps) {
+		log "Retrieving data..."
+		log "    Name,Error,Make,Model,Memory,Serial,BiosVersion"
+		
+		$data = @()
+		
+		$data = $comps | ForEach-Object -ThrottleLimit $ThrottleLimit -Parallel {
+			
+			function log($msg) {
+				Write-Host $msg
+			}
+			
+			$CIMTimeoutSec = $using:CimTimeoutSec
+			$comp = $_
+			
+			$this = $null
+			try {
+				$this = Get-CIMInstance -ClassName "Win32_ComputerSystem" -ComputerName $comp -OperationTimeoutSec $CIMTimeoutSec -ErrorAction "SilentlyContinue"
+				$this2 = Get-CIMInstance -ClassName "Win32_BIOS" -ComputerName $comp -OperationTimeoutSec $CIMTimeoutSec -ErrorAction "SilentlyContinue"
+			}
+			catch {
+			}
+			
+			$err = $null
+			if($this -and $this2) {
+				$this | Add-Member -NotePropertyName "Make" -NotePropertyValue $this.Manufacturer
+				$this | Add-Member -NotePropertyName "Memory" -NotePropertyValue "$([math]::round($this.TotalPhysicalMemory / 1MB))MB"
+				$this | Add-Member -NotePropertyName "Serial" -NotePropertyValue $this2.SerialNumber
+				$this | Add-Member -NotePropertyName "BIOS" -NotePropertyValue $this2.SMBIOSBIOSVersion
+			}
+			else {
+				$err = $true
+				$this = [PSCustomObject]@{
+					"Name" = $comp
+				}
+			}
+			$this | Add-Member -NotePropertyName "Error" -NotePropertyValue $err
+			
+			log "    $($this.Name),$($this.Error),$($this.Make),$($this.Model),$($this.Memory),$($this.Serial),$($this.BIOS)"
+			
+			$this
+		}
+		log "Done retrieving data."
+			
+		$data
+	}
+	
+	function Print-Data($data) {
+		log ($data | Sort Name | Select Name,Error,Make,Model,Memory,Serial,BIOS | Format-Table -AutoSize -Wrap | Out-String).Trim() -NoTS
+	}
+	
+	function Output-Csv($data) {
+		log "-Csv was specified. Outputting gathered data to `"$CSVPATH`"..."
+		$data | Sort Name | Select Name,Error,Make,Model,Memory,Serial,BIOS | Export-Csv -Path $CSVPATH -NoTypeInformation -Encoding Ascii
+		log "Done."
+		
+	}
+	
+	log " " -NoTS
+	
+	$comps = Get-Comps
+	$data = Get-Data $comps
+	log " " -NoTS
+	Print-Data $data
+	log " " -NoTS
+	if($Csv) {
+		Output-Csv $data
+	}
+	log " " -NoTS
+	log "EOF"
+	log " " -NoTS
+}
--- README.md
+++ README.md
@@ -1,2 +1,56 @@
-# Get-MachineInfo
-This script reports various inventory-related info from a list of remote computers by polling them asynchronously.
+# Summary
+This script reports various inventory-related info from a list of remote computers by polling them asynchronously.  
+It optionally outputs a log and a CSV file of the data.  
+
+This is an update of [Get-Model](https://github.com/engrit-illinois/Get-Model) primarily just to add the asynchronicity feature. This relies on the new `-Parallel` parameter of the `ForEach-Object` cmdlet, which makes this incompatible with PowerShell 5.1, a limitation not present in `Get-Model`.  
+
+# Usage
+1. Download `Get-MachineInfo.psm1` to `$HOME\Documents\WindowsPowerShell\Modules\Get-MachineInfo\Get-MachineInfo.psm1`
+2. Run it using the examples and parameter documentation below.
+
+# Examples
+- `Get-MachineInfo "espl-114-01"`
+- `Get-MachineInfo "espl-114-01","espl-114-02","tb-207-01"`
+- `Get-MachineInfo "espl-114-*"`
+- `Get-MachineInfo "espl-114-*","tb-207-01","tb-306-*"`
+- `Get-MachineInfo -OUDN "OU=Instructional,OU=Desktops,OU=Engineering,OU=Urbana,DC=ad,DC=uillinois,DC=edu" -ComputerName "*" -LogDir "c:\engrit\logs" -Log -Csv`
+
+# Parameters
+
+### -ComputerName [string[]]
+Required string array.  
+The list of computer names and/or computer name query strings to poll.  
+Use an asterisk (`*`) as a wildcard.  
+The parameter name may be omitted if the value is given as the first or only parameter.   
+
+### -OUDN [string]
+Optional string.  
+The distinguished name of the OU to limit the computername search to.  
+Default is `"OU=Desktops,OU=Engineering,OU=Urbana,DC=ad,DC=uillinois,DC=edu"`.  
+
+### -Log
+Optional switch.  
+Whether or not to log output to a log file.  
+Log filename will be `Get-DiskSpace_yyyy-MM-dd_HH-mm-ss.log`.  
+Log will be created in the directory specified by the `-LogDir` parameter.  
+
+### -Csv
+Optional switch.  
+Whether or not to log retrieved data to a CSV file.  
+CSV filename will be `Get-DiskSpace_yyyy-MM-dd_HH-mm-ss.csv`.  
+CSV will be created in the directory specified by the `-LogDir` parameter.  
+
+### -LogDir [string]
+Optional string.  
+The directory in which to create log and/or CSV files, if any are created.  
+Default is `"c:\engrit\logs"`.  
+
+### -ThrottleLimit [int]
+Optional integer.  
+The maximum number of computers which will be asynchronously polled simultaneously.  
+Default is `50`.  
+
+# Notes
+- Machines for which data could not be retrieved will have an `Error` value of `TRUE` and will have null data otherwise.
+- Machines for which data was retrieved will have a null `Error` value.
+- By mseng3. See my other projects here: https://github.com/mmseng/code-compendium.
